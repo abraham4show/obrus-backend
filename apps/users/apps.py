2cf5f4ebb0365c2d430848a1d5601b3b4dd738fb
afolabi shorinwa <shorinwaafolabi@gmail.com>
2026-05-03 12:41:18 +0100

    Add signal to create default admin on migration

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -1,6 +1,28 @@
 from django.apps import AppConfig
-
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.users'
+
+
+
+def create_default_admin(sender, **kwargs):
+    from .models import User, UserRole
+    if not User.objects.filter(email='admin@example.com').exists():
+        admin = User.objects.create_superuser(
+            email='admin@example.com',
+            password='Admin123!',
+            first_name='Admin',
+            last_name='User',
+            is_staff=True,
+            is_superuser=True
+        )
+        UserRole.objects.get_or_create(user=admin, role='admin')
+
+class UsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.users'
+    def ready(self):
+        post_migrate.connect(create_default_admin, sender=self)
