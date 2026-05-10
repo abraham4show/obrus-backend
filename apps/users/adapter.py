e0825323b6b6aa2a2a647c44b1043787a44948c3
afolabi shorinwa <shorinwaafolabi@gmail.com>
2026-05-10 23:11:55 +0100

    Fix adapter filename and content

--- apps/users/adapter.py
+++ apps/users/adapter.py
@@ -1,21 +1,24 @@
+import logging
+logger = logging.getLogger(__name__)
+from users.models import User
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def populate_user(self, request, sociallogin, data):
-        user = super().populate_user(request, sociallogin, data)
-        user.email = data.get('email')
-        user.first_name = data.get('first_name', '')
-        user.last_name = data.get('last_name', '')
-        # Ensure username is unique
-        if not user.username:
-            base_username = user.email.split('@')[0]
-            username = base_username
-            counter = 1
-            while User.objects.filter(username=username).exists():
-                username = f"{base_username}{counter}"
-                counter += 1
-            user.username = username
-        return user
+        try:
+            user = super().populate_user(request, sociallogin, data)
+            user.email = data.get('email')
+            user.first_name = data.get('first_name', '')
+            user.last_name = data.get('last_name', '')
+            if not user.username:
+                base_username = user.email.split('@')[0]
+                username = base_username
+                counter = 1
+                while User.objects.filter(username=username).exists():
+                    username = f"{base_username}{counter}"
+                    counter += 1
+                user.username = username
+            return user
+        except Exception as e:
+            logger.error(f"Error in populate_user: {e}", exc_info=True)
+            raise
